a433618bccf044c771547773eb5da09d26f376f3
Roy Meissner <meissner@informatik.uni-leipzig.de>
2024-01-30 14:25:28 +0100

    Added initial program

---
+++ main.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import streamlit as st
+import tempfile
+import os
+
+st.set_page_config(layout="wide")
+st.title('Whisper Audio Transcription')
+
+api_key = st.text_input("OpenAI API Key:", value=None)
+
+file = st.file_uploader("Audio-File", type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav', 'mpeg'], help="File must be less than 25MB")
+
+language = st.selectbox("Language:", ['de', 'en'])
+
+def transcribe_audio(api_key, audio_file, language):
+  file_extension = os.path.splitext(audio_file.name)[-1]
+  
+  tmp = tempfile.NamedTemporaryFile(delete=False, suffix=file_extension)
+  with tmp as temp_audio_file:
+    temp_audio_file.write(audio_file.read())
+    temp_audio_file.seek(0)  # Move the file pointer to the beginning of the file
+    
+  audio= open(tmp.name, "rb")
+  client = OpenAI(api_key=api_key)
+  transcript = client.audio.transcriptions.create(model="whisper-1", file=audio, language=language)
+  return transcript
+
+if st.button('Transcribe') and api_key is not None and file:
+  with st.spinner('Processing...'):
+    transcript = transcribe_audio(api_key, file, language)
+    st.write(transcript.text)
