9a02fa59be31db5b2ebb78ea4bf2f649bde9265d
Roy Meissner <meissner@informatik.uni-leipzig.de>
2024-01-30 14:40:26 +0100

    Deleting uploaded files again

--- main.py
+++ main.py
@@ -20,9 +20,16 @@ def transcribe_audio(api_key, audio_file, language):
     temp_audio_file.write(audio_file.read())
     temp_audio_file.seek(0)  # Move the file pointer to the beginning of the file
     
-  audio= open(tmp.name, "rb")
+  print (tmp.name)
+  audio = open(tmp.name, "rb")
   client = OpenAI(api_key=api_key)
   transcript = client.audio.transcriptions.create(model="whisper-1", file=audio, language=language)
+  try:
+    audio.close()
+    tmp.close()
+    os.unlink(tmp.name)
+  except:
+    print("Problem unlinking file")
   return transcript
 
 if st.button('Transcribe') and api_key is not None and file:
